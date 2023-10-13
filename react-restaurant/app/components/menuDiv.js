826bef5293f088aa7f76a5d1251a3f64c703d2ac
Dylan Wyrick <xpoweroutage@gmail.com>
2023-10-13 09:59:21 -0400

    code freeze

--- react-restaurant/app/components/menuDiv.js
+++ react-restaurant/app/components/menuDiv.js
@@ -1,27 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 
 function MenuDiv(props) {
-  console.log(props.menuItems);
+  // Filter the menu items to get only the first 6 appetizers
+  const appetizers = props.menuItems
+    .filter((menuItem) => menuItem.category === "Appetizer")
+    .slice(0, 6);
+
   return (
     <div
       id="mainContainDiv"
       className="overflow-y-auto mt-5 border border-secondary p-3"
     >
       <div>
-        <h1 className="text-center mb-0 p-4 pb-5">OUR MENU</h1>
+        <h2 className="text-center mb-0 p-4 pb-5">OUR MENU</h2>
       </div>
       <div className="border-top border-bottom mx-4">
-        <p className="m-0 p-3 ps-0">APPETIZERS</p>
+        <h5 className="m-0 p-3 ps-0">APPETIZERS</h5>
       </div>
 
       <div className="mt-4">
-        {props.menuItems.map((menuItem, index) => (
+        {appetizers.map((menuItem, index) => (
           <div key={index}>
-            <h2>{menuItem.title}</h2>
+            <h6>{menuItem.title}</h6>
             <p>{menuItem.price}</p>
             <p>{menuItem.description}</p>
           </div>
         ))}
+      </div>
+
+      <div className="border-top border-bottom mx-4">
+        <h5 className="m-0 p-3 ps-0">LUNCH</h5>
+      </div>
+
+      <div>
+        <p className="mt-4">WE DON"T SERVE LUNCH</p>
       </div>
     </div>
   );
--- react-restaurant/app/page.js
+++ react-restaurant/app/page.js
@@ -1,34 +1,30 @@
-"use client"
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import Navbar from './components/navbar'
-import MenuDiv from './components/menuDiv'
-
-
+"use client";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Navbar from "./components/navbar";
+import MenuDiv from "./components/menuDiv";
 
 export default function Home() {
-  const [menuItems, setMenuItems] = useState([])
+  const [menuItems, setMenuItems] = useState([]);
 
   // function axiosCall() {
-    useEffect(() => {
-        axios.get('https://www.jsonkeeper.com/b/MDXW').then((response) => {
-          console.log(response)
-          setMenuItems(response.data);
-        }); 
-    },[])
+  useEffect(() => {
+    axios.get("https://www.jsonkeeper.com/b/MDXW").then((response) => {
+      console.log(response);
+      setMenuItems(response.data);
+    });
+  }, []);
 
-    console.log(menuItems)
-    
+  console.log(menuItems);
+
   return (
-    <div style={{}}>
-    <div className="container ms-5">
-      <Navbar />
-      <MenuDiv menuItems={menuItems} />
-      
-    </div>
-    </div>
+    <>
+      <div className="container">
+        <Navbar />
+        <MenuDiv menuItems={menuItems} />
+      </div>
+    </>
   );
 }
-
